ee90fc8b4b0e36704897f00c26b0198e2f11c9b2
isaac-fei <isaac.omega.fei@gmail.com>
2025-01-07 12:01:23 +0800

    study outline

--- src/components/pdf-viewer/pdf-viewer.tsx
+++ src/components/pdf-viewer/pdf-viewer.tsx
@@ -5,12 +5,12 @@ import { useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 
 import * as pdfjsLib from "pdfjs-dist";
-import { TextLayerBuilder } from "pdfjs-dist/web/pdf_viewer.mjs";
-import "@/styles/text_layer_builder.css";
+// import "pdfjs-dist/web/pdf_viewer.css";
+import "@/styles/pdf-viewer.css";
 
 // Set the worker
 pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
+  "pdfjs-dist/build/pdf.worker.js",
   import.meta.url,
 ).toString();
 
@@ -42,11 +42,17 @@ export function PDFViewer({ className, src }: PDFViewerProps) {
     )
       return;
 
+    const outline = await pdf.getOutline();
+    console.debug("outline", outline);
+
     // Get the page
     const page = await pdf.getPage(pageNumber);
 
+    const destination = await pdf.getDestination("section.2.1");
+    console.debug("destination", destination);
+
     // Get the viewport of the page
-    const viewport = page.getViewport({ scale: 1.5 });
+    const viewport = page.getViewport({ scale: 1 });
 
     // Get the canvas element
     const canvas = canvasRef.current;
@@ -71,18 +77,12 @@ export function PDFViewer({ className, src }: PDFViewerProps) {
 
     // Render the text layer
     const textContent = await page.getTextContent();
-
-    console.debug("textContent", textContent);
-
-    // Create the text layer instance
-    const textLayer = new pdfjsLib.TextLayer({
+    pdfjsLib.renderTextLayer({
       textContentSource: textContent,
       container: textLayerRef.current,
       viewport: viewport,
+      textDivs: [],
     });
-
-    // Render the text layer
-    await textLayer.render();
   }
 
   useEffect(() => {
@@ -96,15 +96,10 @@ export function PDFViewer({ className, src }: PDFViewerProps) {
   }, [pdf]);
 
   return (
-    <div
-      className={cn(
-        "relative h-full w-full overflow-clip rounded-md border-2",
-        className,
-      )}
-    >
+    <div className={cn("pdfViewer relative", className)}>
       <canvas ref={canvasRef} className="border-2"></canvas>
 
-      <div ref={textLayerRef} className="textLayer !z-50"></div>
+      <div ref={textLayerRef} className="textLayer [&_span]:!text-white"></div>
 
       <div ref={annotationLayerRef} className="annotationLayer"></div>
     </div>
